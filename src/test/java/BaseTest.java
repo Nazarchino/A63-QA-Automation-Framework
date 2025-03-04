796668096a1b6ebb56397bf97a37f6d950e4c4ef
Nazarchino <nazarchino@gmail.com>
2025-03-04 15:43:05 +0200

    upd

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -15,6 +16,7 @@ import org.testng.annotations.*;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.time.Duration;
+import java.util.HashMap;
 
 public class BaseTest {
 
@@ -60,14 +62,25 @@ public class BaseTest {
                 options.addArguments("--remote-allow-origins=*");
                 options.addArguments("--disable-notifications");
                 return driver = new ChromeDriver(options);
-            case "safari":
-                WebDriverManager.safaridriver().setup();
-                return driver = new SafariDriver();
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return driver = new EdgeDriver();
             case "grid":
-                capabilities.setCapability("browserName", "safari");
+                capabilities.setCapability("browserName", "edge");
                 return driver = new RemoteWebDriver(URI.create(gridUrl).toURL(), capabilities);
-//            case "lambda":
-//                return getLambdaDriver();
+            case "lambda":
+                String hub = "@hub.lambdatest.com/wd/hub";
+                String userName = "nazarchino";
+                String apiKey = "LT_dMHpzRbcLO7GJJiv2sQ5EnurzDcDeYOm8TJSnHNgLTK3A2Z";
+                capabilities.setCapability("browserName", "Chrome");
+                capabilities.setCapability("browserVersion", "133.0");
+                HashMap<String, Object> ltOptions = new HashMap<>();
+                ltOptions.put("username", userName);
+                ltOptions.put("apiKey", apiKey);
+                ltOptions.put("platformName", "Windows 10");
+                ltOptions.put("project", "Koel");
+                capabilities.setCapability("LT:Options", ltOptions);
+                return driver = new RemoteWebDriver(URI.create("https://" + userName + ";" + apiKey + hub).toURL(), capabilities);
             default:
                 WebDriverManager.chromedriver().setup();
                 options.addArguments("--remote-allow-origins=*");
